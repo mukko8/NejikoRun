51f070f8518a819413ff5f7910b5c1e3bfa39a7b
steel <gawct.231020@gmail.com>
2024-03-19 13:28:42 +0900

    P267

--- Assets/Scripts/NejikoController.cs
+++ Assets/Scripts/NejikoController.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class NejikoController : MonoBehaviour
 {
+    const int MinLane=-2;
+    const int MaxLane=2;
+    const float LaneWidth=1.0f;
     CharacterController controller;
     Animator animator;
 
     Vector3 moveDirection=Vector3.zero;
+    int targetLane;
 
     public float gravity;
     public float speedZ;
+    public float speedX;
     public float speedJump;
+    public float accelerationZ;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,19 +29,19 @@ public class NejikoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(controller.isGrounded){
-            if(Input.GetAxis("Vertical")>0.0f){
-                moveDirection.z=Input.GetAxis("Vertical")*speedZ;
-            }else{
-                moveDirection.z=0;
-            }
-            transform.Rotate(0,Input.GetAxis("Horizontal")*3,0);
+        //デバッグ用
+        if(Input.GetKeyDown("left")) MoveToLeft();
+        if(Input.GetKeyDown("right")) MoveToRight();
+        if(Input.GetKeyDown("space")) Jump();
 
-            if(Input.GetButton("Jump")){
-                moveDirection.y=speedJump;
-                animator.SetTrigger("jump");
-            }
-        }
+        //徐々に加速しZ方向に常に前進させる
+        float acceleratedZ=moveDirection.z +(accelerationZ*Time.deltaTime);
+        moveDirection.z = Mathf.Clamp(acceleratedZ,0,speedZ);
+
+        //X方向は目的のポジションまでの差分の割合で速度を計算
+        float ratioX=(targetLane * LaneWidth - transform.position.x) / LaneWidth;
+        moveDirection.x = ratioX * speedX;
+
         //重力分の力のマイフレームを追加
         moveDirection.y -= gravity * Time.deltaTime;
         //移動実行(ネジコの向きを考慮したベクトルに変換)
@@ -46,5 +52,21 @@ public class NejikoController : MonoBehaviour
         if(controller.isGrounded) moveDirection.y = 0;
         //速度が0以上なら走っているフラグをtrueにする
         animator.SetBool("run",moveDirection.z > 0f);
+    }
+    //左のレーンに移動を開始
+    public void MoveToLeft(){
+        if(controller.isGrounded&&targetLane>MinLane) targetLane--;
+    }
+    //右のレーンに移動を開始
+    public void MoveToRight(){
+        if(controller.isGrounded&&targetLane<MaxLane) targetLane++;
+    }
+    public void Jump(){
+        if(controller.isGrounded){
+            moveDirection.y=speedJump;
+
+            //ジャンプトリガーを設定
+            animator.SetTrigger("jump");
+        }
     }
 }

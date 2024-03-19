2606312116a9ce1ea82261a498e7b90c33ebd5ee
steel <gawct.231020@gmail.com>
2024-03-19 10:48:32 +0900

    P252

---
+++ Assets/Scripts/NejikoController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NejikoController : MonoBehaviour
+{
+    CharacterController controller;
+    Animator animator;
+
+    Vector3 moveDirection=Vector3.zero;
+
+    public float gravity;
+    public float speedZ;
+    public float speedJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //必要なコンポーネントを自動取得
+        controller=GetComponent<CharacterController>();
+        animator=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controller.isGrounded){
+            if(Input.GetAxis("Vertical")>0.0f){
+                moveDirection.z=Input.GetAxis("Vertical")*speedZ;
+            }else{
+                moveDirection.z=0;
+            }
+            transform.Rotate(0,Input.GetAxis("Horizontal")*3,0);
+
+            if(Input.GetButton("Jump")){
+                moveDirection.y=speedJump;
+                animator.SetTrigger("Jump");
+            }
+        }
+        //重力分の力のマイフレームを追加
+        moveDirection.y -= gravity * Time.deltaTime;
+        //移動実行
+        Vector3 globalDirection = transform.TransformDirection(moveDirection);
+        controller.Move(globalDirection*Time.deltaTime);
+
+        //移動後接地したらy方向の速度はリセットする
+        if(controller.isGrounded) moveDirection.y = 0;
+        //速度が0以上なら走っているフラグをtrueにする
+        animator.SetBool("run",moveDirection.z > 0f);
+    }
+}
